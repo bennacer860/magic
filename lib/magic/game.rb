b3e2feef40ebf52fa9bbc7fca2fdf6777bd33233
Londerson <londerson@gmail.com>
2014-12-12 09:09:54 -0200

    create files

---
+++ lib/magic/game.rb
@@ -0,0 +1,5 @@
+module Magic
+  class Game
+
+  end
+end
---
+++ spec/magic/game_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require_relative './../../lib/magic/game.rb'
+
+describe Magic::Game do
+  it "create one game" do
+
+  end
+end
