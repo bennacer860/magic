efd83625927d8f5833edf36a6cf6195671a867a9
Londerson <londerson@gmail.com>
2014-12-12 10:09:29 -0200

    config mongoid

--- lib/magic/game.rb
+++ lib/magic/game.rb
@@ -1,5 +1,17 @@
+require 'mongoid'
+ENV["RACK_ENV"] = "development"
+Mongoid.load!("./../../db/mongoid.yml")
+
+
 module Magic
   class Game
+    include Mongoid::Document
 
+    def show
+      p Game.count
+    end
   end
 end
+
+m = Magic::Game.new
+m.show
--- spec/magic/game_spec.rb
+++ spec/magic/game_spec.rb
@@ -3,6 +3,12 @@ require_relative './../../lib/magic/game.rb'
 
 describe Magic::Game do
   it "create one game" do
+    game = Magic::Game.new
+    expect(game).to be_an_instance_of(Magic::Game)
+  end
 
+  it "bring players to game" do
+    game = Magic::Game.new
+    expect(game.players).to eq([player1, player2])
   end
 end
