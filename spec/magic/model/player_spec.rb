7b303c081e2f8e09d788bf662bcbbf0117af3a34
Londerson <londerson@gmail.com>
2014-12-12 16:18:02 -0200

    test if player has instance of Player class

--- spec/magic/model/player_spec.rb
+++ spec/magic/model/player_spec.rb
@@ -1,3 +1,13 @@
 require 'rspec'
 require_relative './../../../config/spec_helper.rb'
 require_relative './../../../lib/magic/model/player.rb'
+
+describe Magic::Model::Player do
+  before :each do
+    @player = Magic::Model::Player.create(name: "John")
+  end
+
+  it "return an instance of Player" do
+    expect(@player).to be_an_instance_of(Magic::Model::Player)
+  end
+end
