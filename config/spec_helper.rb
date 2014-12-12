a8feb9c3cfd70043ce5d11fd0bcfff2d2ab7c097
Londerson <londerson@gmail.com>
2014-12-12 15:31:58 -0200

    config database cleaner

--- config/spec_helper.rb
+++ config/spec_helper.rb
@@ -1,4 +1,25 @@
 require 'mongoid'
+require 'database_cleaner'
 
 ENV["RACK_ENV"] = "development"
 Mongoid.load!("./db/mongoid.yml")
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  #config database cleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.orm = "mongoid"
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.clean
+  end
+end
