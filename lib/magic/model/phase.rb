768787240b1cdd8f9f734e0bcfd36713fa745972
Londerson <londerson@gmail.com>
2014-12-12 17:26:35 -0200

    create content phases and array

--- lib/magic/model/phase.rb
+++ lib/magic/model/phase.rb
@@ -4,6 +4,22 @@ module Magic
       include Mongoid::Document
       belongs_to :turn
 
+      UNTAP        = "untap"
+      UPKEEP       = "upkeep"
+      DRAW         = "draw"
+      MAIN         = "main"
+      BEGIN_COMBAT = "begin_combat"
+      ATTACK       = "attack"
+      BLOCK        = "block"
+      DAMAGE       = "damage"
+      END_COMBAT   = "end_combat"
+      SECOND_MAIN  = "second_main"
+      SECOND_END   = "second_end"
+      CLEANUP      = "cleanup"
+
+      PHASES       = [UNTAP, UPKEEP, DRAW, MAIN, BEGIN_COMBAT, ATTACK, BLOCK, 
+                      DAMAGE, END_COMBAT, SECOND_MAIN, SECOND_END, CLEANUP]
+
       field :name, type: String
     end
   end
