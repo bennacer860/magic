b373bd8510db5ba585954fdf2edaf94254dd2726
Londerson <londerson@gmail.com>
2014-12-17 10:12:58 -0200

    config factory girl

--- config/spec_helper.rb
+++ config/spec_helper.rb
@@ -6,13 +6,14 @@ ENV["RACK_ENV"] = "development"
 Mongoid.load!("./db/mongoid.yml")
 
 RSpec.configure do |config|
-
+  #config factory girl
+  config.include FactoryGirl::Syntax::Methods
   #config database cleaner
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.orm = "mongoid"
   end
-
+  #config database cleaner
   config.before(:each) do
     DatabaseCleaner.clean
   end
