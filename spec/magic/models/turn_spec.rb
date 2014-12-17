a4011e45e04a24341eb344ba2c4425a5d5af0869
Londerson <londerson@gmail.com>
2014-12-17 14:16:12 -0200

    refac and create factory

--- spec/magic/models/turn_spec.rb
+++ spec/magic/models/turn_spec.rb
@@ -2,10 +2,10 @@ require_relative './../../../config/spec_helper.rb'
 
 describe Turn do
   before :each do
-    @turn = Turn.create
+    subject { create :turn }
   end
 
   it "return a new instance" do
-    expect(@turn).to be_an_instance_of(Turn)
+    expect(subject).to be_an_instance_of(Turn)
   end
 end
