cf73af189e1d806498855dba70d427b6ece39921
Londerson <londerson@gmail.com>
2014-12-26 10:38:47 -0200

    create main window

---
+++ lib/magic/client/main.rb
@@ -0,0 +1,19 @@
+require 'gosu'
+
+class Main < Gosu::Window
+
+  def initialize
+    super 640, 480, false
+    self.caption = "Magic"
+  end
+
+  def update
+    
+  end
+
+  def draw
+    
+  end
+end
+
+Main.new.show
