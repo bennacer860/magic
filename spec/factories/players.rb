beb5594f8394f6bf09718166a06e11659fb1b491
Londerson <londerson@gmail.com>
2014-12-26 10:38:23 -0200

    create factory match

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,5 +1,15 @@
 FactoryGirl.define do
   factory :player do
     name "Joe"
+
+    factory :player_with_one_turn do
+      after(:create) do |player|
+        create(:turn, player: player)
+      end
+    end
+  end
+
+  factory :p1 do
+    name "Blah"
   end
 end
