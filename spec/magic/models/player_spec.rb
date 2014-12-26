b95f6086983fd21789d1af1b94711b4a9b4b07c6
Londerson <londerson@gmail.com>
2014-12-26 10:38:36 -0200

    refac test

--- spec/magic/models/player_spec.rb
+++ spec/magic/models/player_spec.rb
@@ -2,10 +2,16 @@ require_relative './../../../config/spec_helper.rb'
 
 describe Player do
   before :each do
-    subject { create(:player) }
+    match = Match.create
+    @player = create(:player_with_one_turn, match_id: match.id)
+    subject { create(:p1)}
   end
 
-  it "return an instance of Player" do
-    expect(subject).to be_an_instance_of(Player)
+  it "is one instance of Player" do
+    expect(@player).to be_an_instance_of(Player)
+  end
+
+  it "have turns" do
+    expect(@player.turns.size).to eq(1)
   end
 end
